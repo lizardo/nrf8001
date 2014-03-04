31ada53f0fe73b947f345ace9e8b7f4cda2da98a
Anderson Lizardo <anderson.lizardo@openbossa.org>
2014-03-04 16:16:20 -0400

    tap.sh: Fix not removing files from previous test

--- tests/nrfgostudio/common/tap.sh
+++ tests/nrfgostudio/common/tap.sh
@@ -9,6 +9,7 @@ run_test()
     name=$2
     desc=$3
     $SHELL -e -c "
+    rm -rf $tmpdir/{expected,actual}
     cp -a $(basename $0 .sh)/$name $tmpdir/expected
     ./common/run.sh $tmpdir/actual $(basename $0 .sh)/${name}.au3
     sed -i '/^ Generated:/d' $tmpdir/{expected,actual}/ublue_setup.gen.out.txt
